2d0fcca6594825e33bff05b21c886b95412f7e01
Jan Busk <jan@fjb.sewq>
2022-02-24 08:18:32 +0100

    Adde colourized print out and added leading zeros

--- decitime.py
+++ decitime.py
@@ -10,14 +10,20 @@ def convert_timestamp_deciseconds():
     make_it_seconds = dt.hour*3600 + dt.minute * 60 + dt.second
     decitime=int(make_it_seconds*time_constant)
     deconds = int(round((decitime / 100 - int(decitime / 100)) * 100, 0))
+    #deconds = int((decitime / 100 - int(decitime / 100)) * 100)
     dinuts = int((decitime / 10000 - int(decitime / 10000)) * 100)
     dours = int((decitime/100000 - int(decitime / 100000)) * 10)
     if dours == 10:
         dours = 0
-    time_string = str(dours) + ":" + str(dinuts) + ":" + str(deconds)
+    time_string = "\033[1;35m" + set_leading_zero(str(dours)) + ":" + set_leading_zero(str(dinuts)) + ":" + set_leading_zero(str(deconds))
     return time_string
 
 
+def set_leading_zero(string):
+    string = string.zfill(2)
+    return string
+
 while True:
     print(convert_timestamp_deciseconds(), end='\r')
-    time.sleep(0.864)
+    #time.sleep(0.864)
+    time.sleep(0.108)
